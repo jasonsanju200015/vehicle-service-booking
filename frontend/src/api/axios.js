5ccd1e09c6e9400b0f99061d9724aef8889ab639
jasonsanju200015 <jasonsanju2000@gmail.com>
2026-03-01 22:23:10 +0530

    Hardcode backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-const api = axios.create({ baseURL: API_URL + '/api' });
+const api = axios.create({ baseURL: 'https://vehicle-service-booking-gu5w.onrender.com/api' });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
