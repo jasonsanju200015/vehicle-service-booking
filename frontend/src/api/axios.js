276d86e271b0ec851b65ae2d790c3b782a153175
jasonsanju200015 <jasonsanju2000@gmail.com>
2026-03-01 22:20:26 +0530

    Fix API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: (process.env.REACT_APP_API_URL || 'http://localhost:5000') + '/api' });
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const api = axios.create({ baseURL: API_URL + '/api' });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
